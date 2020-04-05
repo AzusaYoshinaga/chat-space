42ccdd6bea7c9310503293dcb4ffbe61ce679465
AzusaYoshinaga <AzusaYoshinaga@gmail.com>
2020-04-05 22:29:32 +0900

    Revert "Saveボタンの調整"
    This reverts commit 8191a98eaa2baade7e23efb05aef0867238405b9.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-      @users = User.where('name LIKE(?)', "%#{params[:name]}%").where.not(id: current_user.id).where.not(id: params[:user_ids])
+      @users = User.where('name LIKE(?)', "%#{params[:name]}%").where.not(id: current_user.id)
       respond_to do |format|
           format.html
           format.json
