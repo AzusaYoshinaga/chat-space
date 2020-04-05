8191a98eaa2baade7e23efb05aef0867238405b9
AzusaYoshinaga <AzusaYoshinaga@gmail.com>
2020-04-05 20:18:01 +0900

    Saveボタンの調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-      @users = User.where('name LIKE(?)', "%#{params[:name]}%").where.not(id: current_user.id)
+      @users = User.where('name LIKE(?)', "%#{params[:name]}%").where.not(id: current_user.id).where.not(id: params[:user_ids])
       respond_to do |format|
           format.html
           format.json
